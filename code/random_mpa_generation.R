55dbe95232295115028e8ec4c492f497d1e6db82
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2022-03-10 12:08:08 -0400

    fixed the stratification of sites

--- code/random_mpa_generation.R
+++ code/random_mpa_generation.R
@@ -21,6 +21,7 @@ bathy <- read_sf("data/shapefiles/Countour_250.shp")%>%
 network_raw <- read_sf("data/shapefiles/networksites_proposed_OEM_MPA_v2.shp")
 
 network <- network_raw%>%
+  filter(!NAME %in% c("Lophelia Coral Conservation Area","Northeast Channel Coral Conservation Area"))%>%
   group_by(NAME)%>%
   summarise(geometry=st_union(geometry))%>% #the shape files has Western Emerald Bank as two sites
   left_join(.,network_raw%>%
@@ -32,7 +33,10 @@ network <- network_raw%>%
   dplyr::select(NAME,STATUS,TYPE,area,species,geometry)%>%st_transform(latlong)
 
 # this of course can be any other grid, but this is for demonstration
-grid <- st_make_grid(bioregion,n=100,square=FALSE) %>% 
+
+ncells=100
+
+grid <- st_make_grid(bioregion,n=ncells,square=FALSE) %>% 
   st_as_sf() %>% 
   st_filter(bioregion)
 
@@ -134,28 +138,57 @@ ggplot(grid)+
 
 #### stratififed ####
 #yes this is slow... and manually fixes Georges Basin
-grid_stratified <- grid %>% 
-  mutate(coastal=lengths(st_intersects(.,simplecoastal))>0,
-         bathy=lengths(st_intersects(.,bathy))>0,
-         strata=case_when(coastal ~ "coastal",
-                          !coastal&bathy ~ "mid-shore",
-                          !coastal&!bathy ~ "offshore")) %>%
-  mutate(strata=if_else(strata == "offshore" & # hard coded override for Georges Basin = mid-shore
-                          st_coordinates(st_centroid(x))[,1]<(-66) &
-                          st_coordinates(st_centroid(x))[,2]>42,
-                        "mid-shore",
-                        strata))
+if(file.exists(paste0("data/stratified_grid_",ncells,".shp"))){grid_stratified <- st_read(paste0("data/stratified_grid_",ncells,".shp"))}else{
+  
+  grid_stratified <- grid %>% 
+    mutate(coastal=lengths(st_intersects(.,simplecoastal))>0,
+           bathy=lengths(st_intersects(.,bathy))>0,
+           strata=case_when(coastal ~ "coastal",
+                            !coastal&bathy ~ "mid-shore",
+                            !coastal&!bathy ~ "offshore")) %>%
+    mutate(strata=if_else(strata == "offshore" & # hard coded override for Georges Basin = mid-shore
+                            st_coordinates(st_centroid(x))[,1]<(-66) &
+                            st_coordinates(st_centroid(x))[,2]>42,
+                          "mid-shore",
+                          strata))
+  
+  #write it so you don't have to repeat
+  st_write(grid_stratified,paste0("data/stratified_grid_",ncells,".shp"))
+  
+} #end if else 
+
+#make grid stratified a polygon 
+grid_stratified_group <- grid_stratified%>%
+  group_by(strata)%>%
+  summarise(geometry=st_union(geometry))%>%
+  ungroup()
+
 
 ggplot(grid_stratified)+
   geom_sf(aes(fill=strata))
 
 # calculate the area in each mpa
-network_stratified <- grid_stratified %>% 
-  group_by(strata) %>% 
-  summarize() %>% 
-  st_intersection(network)
 
-areas <- as.numeric(st_area(network_stratified))
+network_cents <- network%>%
+  st_centroid() #keep for plotting
+
+network_cents_stratified <- network%>%
+  st_centroid()%>%
+  st_intersection(grid_stratified)
+
+network_stratified <- network%>%
+  left_join(.,network_cents_stratified%>%data.frame()%>%select(NAME,strata))%>%
+  mutate(area=as.numeric(st_area(geometry)))
+
+
+ggplot()+
+  geom_sf(data=grid_stratified_group,aes(fill=strata))+
+  geom_sf(data=grid_stratified%>%filter(strata=="mid-shore"),fill=NA)+ #show the grid here
+  geom_sf(data=network_stratified,aes(fill=strata))+
+  geom_sf(data=network_cents_stratified)+
+  theme_bw()
+
+areas <- network_stratified$area
 
 rand_stratified_mpa <- function(grid,areas,grid_strata,area_strata,adj_edgelist,buff_edgelist=adj_edgelist,max_edge=4){
   purrr::map(unique(grid_strata),function(s){
@@ -168,6 +201,25 @@ rand_stratified_mpa <- function(grid,areas,grid_strata,area_strata,adj_edgelist,
 }
 
 newnetwork <- rand_stratified_mpa(grid,areas,grid_stratified$strata,network_stratified$strata,adj_edgelist,buff_edgelist,max_edge=4)
+
+newnetwork_grouped <- newnetwork%>%
+  group_by(mpa_id)%>%
+  summarise(geometry=st_union(x))%>%
+  ungroup()%>%
+  mutate(area=as.numeric(st_area(geometry)))%>%
+  arrange(area)%>%
+  suppressMessages()
+
+grid_grouped <- grid_stratified%>%
+                group_by(strata)%>%
+                summarise(geometry=st_union(geometry))%>%
+                ungroup()
+
+ggplot()+
+  geom_sf(data=newnetwork_grouped,aes(fill=factor(mpa_id)))+
+  geom_sf(data=grid_grouped,fill=NA)+
+  theme_bw()+
+  theme(legend.position='none')
 
 
 ggplot(grid)+
